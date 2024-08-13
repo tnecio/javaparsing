5d57053e7e7c09376b852244b7ba819356bfe6c8
Tomasz Necio <tomasz.necio@gmail.com>
2024-08-13 09:06:49 +0200

    Example parse

--- src/main/java/io/tnec/javaparsing/Main.java
+++ src/main/java/io/tnec/javaparsing/Main.java
@@ -1,7 +1,32 @@
 package io.tnec.javaparsing;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.body.MethodDeclaration;
+
 public class Main {
+    private static final String TEST_CASE = """
+/**
+ * Tests for correct dependency retrieval with maven rules.
+ */
+public class MainTest {
+
+  @Test
+  public void dummyMethod_returns17() throws Exception {
+    assertEquals("should return 17", 17, Main.dummyMethod());
+  }
+  @Test
+  public void dummyMethod_returns18() throws Exception {
+    assertEquals("should return 18", 18, Main.dummyMethod());
+  }
+  }
+    """;
+
     public static void main(String[] args) {
+        var ast = StaticJavaParser.parse(TEST_CASE);
+        ast.findAll(MethodDeclaration.class).forEach(method -> {
+            System.out.println("Method name: " + method.getName());
+            System.out.println("Method body: " + method.getBody().get());
+        });
         System.out.println("Hello, World!");
     }
 
